3bbce660cc4db189228175909aef7f35374b308f
Brittany Morris <brittmmendez@gmail.com>
2018-01-25 01:51:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,14 +11,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(:username => params[:username])
-      if @user != nil && @user.password == params[:password]
-        session[:user_id] = @user.id
-        redirect to '/account'
-    else
-      erb :error
-    end
+  @user = User.find_by(:username => params[:username])
+  if @user != nil && @user.password == params[:password]
+    session[:user_id] = @user.id
+    redirect to '/account'
   end
+  erb :error
+end
 
   get '/account' do
     @current_user = User.find_by_id(session[:user_id])
