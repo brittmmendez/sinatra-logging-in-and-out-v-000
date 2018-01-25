7634148eb66aefc8cb5d0d3803d17518eaee006a
Brittany Morris <brittmmendez@gmail.com>
2018-01-25 01:42:21 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,14 @@
 class Helpers
-def self.current_user(hash)
-  @user = User.find_by(hash[:user_id])
+def self.current_user(session)
+  @user = User.find_by(session[:user_id])
 end
 
-def self.is_logged_in?(hash)
-    !!hash[:user_id]
+def self.is_logged_in?(session)
+    !!session[:user_id]
   end
+<<<<<<< HEAD
 
+=======
+  
+>>>>>>> 117e36221c941fe8ac2adbf2fa1256f71dbefede
 end
